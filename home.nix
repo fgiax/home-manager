0859135c36caf068c8b8eb88cea8a0cbf2c9184c
zuger <fgiax02@gmail.com>
2026-07-08 17:18:19 +0000

    Update Home Manager config

--- home.nix
+++ home.nix
@@ -29,16 +29,14 @@
     '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    ".config/nvim".source = ./dotfiles/nvim;
+  home.extraOutputsToInstall = [
+    "doc"
+    "info"
+    "devdoc"
+  ];
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  home.file = {
+    # ".config/nvim".source = ./dotfiles/nvim;
   };
 
   home.sessionVariables = {
