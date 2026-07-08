bc792b0ab23e6d763748c53a45b87a38d0b9c8fc
zuger <fgiax02@gmail.com>
2026-07-08 07:21:40 +0000

    Update Home Manager config

--- home.nix
+++ home.nix
@@ -11,9 +11,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
     cowsay
     alejandra
     grc
@@ -28,7 +25,7 @@
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
     (pkgs.writeShellScriptBin "hello-me" ''
-      echo "Hello, ${config.home.username}!"
+      cowsay "Hello, ${config.home.username}!"
     '')
   ];
 
